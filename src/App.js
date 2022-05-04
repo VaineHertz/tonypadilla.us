a93668866a076c30539c66eecba20cecc9ee04fe
Tony Padilla <vainehertz@yahoo.com>
2022-05-04 14:26:52 -0700

    finally fixed site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import { FiPhoneCall } from 'react-icons/fi';
+import { FiInstagram } from 'react-icons/fi';
+import { FiMail } from 'react-icons/fi';
+import './fonts/GreatVibes-Regular.ttf';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+      <head>
+		    <title>
+		    	TONY PADILLA
+	    	</title>
+	    </head>
+	    <body>
+          <div className="info">
+            <br/>
+            <a href="https://www.instagram.com/tonypadilla.us/" target='_blank'><FiInstagram style={{ color: 'black'}} /></a>     <a href="mailto:me@tonypadilla.us"><FiMail style={{color:'black'}} /></a>   <a href="tel:2092759460"><FiPhoneCall style={{color:'black'}} /> <span style={{fontSize: '20px', verticalAlign: '10px', color: 'black'}}>(209) 275-9460</span></a>
+          </div>
+          <br />
+          <div className="myName">Tony Padilla</div>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <script src="modal.js"></script>
+	  </body>
+  </div>
   );
 }
 
